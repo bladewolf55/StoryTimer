878c842443cefddc2cdbb21af91d672f149274a7
Charles L Flatt <charles@tfhome.us>
2019-09-19 22:31:51 -0600

    Initial release
    Has all basic functionality.

--- StoryTimer/Form1.Designer.cs
+++ StoryTimer/Form1.Designer.cs
@@ -29,78 +29,122 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBoxTitle1 = new System.Windows.Forms.TextBox();
+            this.richTextBoxElapsedTime = new System.Windows.Forms.RichTextBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonStartPause = new System.Windows.Forms.Button();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
             this.textBoxNew = new System.Windows.Forms.TextBox();
-            this.buttonStartPause1 = new System.Windows.Forms.Button();
-            this.buttonReset1 = new System.Windows.Forms.Button();
-            this.richTextBoxTime1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.richTextBoxTime1);
-            this.panel1.Controls.Add(this.buttonReset1);
-            this.panel1.Controls.Add(this.buttonStartPause1);
-            this.panel1.Controls.Add(this.textBoxTitle1);
-            this.panel1.Location = new System.Drawing.Point(12, 34);
+            this.panel1.Controls.Add(this.richTextBoxElapsedTime);
+            this.panel1.Controls.Add(this.buttonReset);
+            this.panel1.Controls.Add(this.buttonStartPause);
+            this.panel1.Controls.Add(this.textBoxTitle);
+            this.panel1.Location = new System.Drawing.Point(9, 63);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(182, 59);
             this.panel1.TabIndex = 0;
             // 
-            // textBoxTitle1
+            // richTextBoxElapsedTime
             // 
-            this.textBoxTitle1.Location = new System.Drawing.Point(4, 3);
-            this.textBoxTitle1.Name = "textBoxTitle1";
-            this.textBoxTitle1.Size = new System.Drawing.Size(170, 20);
-            this.textBoxTitle1.TabIndex = 0;
+            this.richTextBoxElapsedTime.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.richTextBoxElapsedTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxElapsedTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxElapsedTime.ForeColor = System.Drawing.Color.Orange;
+            this.richTextBoxElapsedTime.Location = new System.Drawing.Point(94, 28);
+            this.richTextBoxElapsedTime.Name = "richTextBoxElapsedTime";
+            this.richTextBoxElapsedTime.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBoxElapsedTime.Size = new System.Drawing.Size(80, 28);
+            this.richTextBoxElapsedTime.TabIndex = 4;
+            this.richTextBoxElapsedTime.Text = " 0:00:00";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReset.Location = new System.Drawing.Point(46, 28);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(42, 28);
+            this.buttonReset.TabIndex = 3;
+            this.buttonReset.Text = "R";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            // 
+            // buttonStartPause
+            // 
+            this.buttonStartPause.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonStartPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStartPause.Location = new System.Drawing.Point(4, 28);
+            this.buttonStartPause.Name = "buttonStartPause";
+            this.buttonStartPause.Size = new System.Drawing.Size(43, 28);
+            this.buttonStartPause.TabIndex = 2;
+            this.buttonStartPause.Text = ">  | |";
+            this.buttonStartPause.UseVisualStyleBackColor = false;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(4, 3);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(170, 20);
+            this.textBoxTitle.TabIndex = 1;
             // 
             // textBoxNew
             // 
-            this.textBoxNew.Location = new System.Drawing.Point(16, 8);
+            this.textBoxNew.Location = new System.Drawing.Point(13, 38);
             this.textBoxNew.Name = "textBoxNew";
             this.textBoxNew.Size = new System.Drawing.Size(170, 20);
-            this.textBoxNew.TabIndex = 1;
-            this.textBoxNew.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            this.textBoxNew.TabIndex = 0;
+            this.textBoxNew.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBoxNew_KeyUp);
             // 
-            // buttonStartPause1
+            // label1
             // 
-            this.buttonStartPause1.Location = new System.Drawing.Point(4, 28);
-            this.buttonStartPause1.Name = "buttonStartPause1";
-            this.buttonStartPause1.Size = new System.Drawing.Size(38, 28);
-            this.buttonStartPause1.TabIndex = 1;
-            this.buttonStartPause1.Text = ">  | |";
-            this.buttonStartPause1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Story Timer";
             // 
-            // buttonReset1
+            // label2
             // 
-            this.buttonReset1.Location = new System.Drawing.Point(53, 28);
-            this.buttonReset1.Name = "buttonReset1";
-            this.buttonReset1.Size = new System.Drawing.Size(35, 28);
-            this.buttonReset1.TabIndex = 3;
-            this.buttonReset1.Text = "R";
-            this.buttonReset1.UseVisualStyleBackColor = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter a title and press <Enter>";
             // 
-            // richTextBoxTime1
+            // checkBox1
             // 
-            this.richTextBoxTime1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBoxTime1.Location = new System.Drawing.Point(100, 28);
-            this.richTextBoxTime1.Name = "richTextBoxTime1";
-            this.richTextBoxTime1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
-            this.richTextBoxTime1.Size = new System.Drawing.Size(74, 28);
-            this.richTextBoxTime1.TabIndex = 4;
-            this.richTextBoxTime1.Text = "0:00:00";
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox1.Location = new System.Drawing.Point(112, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Exclusive";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(203, 105);
+            this.ClientSize = new System.Drawing.Size(203, 128);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxNew);
             this.Controls.Add(this.panel1);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Story Timer";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -111,11 +155,14 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.RichTextBox richTextBoxTime1;
-        private System.Windows.Forms.Button buttonReset1;
-        private System.Windows.Forms.Button buttonStartPause1;
-        private System.Windows.Forms.TextBox textBoxTitle1;
+        private System.Windows.Forms.RichTextBox richTextBoxElapsedTime;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonStartPause;
+        private System.Windows.Forms.TextBox textBoxTitle;
         private System.Windows.Forms.TextBox textBoxNew;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- StoryTimer/Form1.cs
+++ StoryTimer/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,14 +13,226 @@ namespace StoryTimer
 {
     public partial class Form1 : Form
     {
+        List<StoryTimerInstance> _storyTimers = new List<StoryTimerInstance>();
         public Form1()
         {
             InitializeComponent();
+            panel1.Visible = false;
+            this.Height = this.Height - panel1.Height;
+            checkBox1.Checked = true;
+
+            StoryTimerInstance storyTimer = new StoryTimerInstance(_storyTimers.Count, panel1);
+            storyTimer.TimerStarted += StoryTimer_TimerStarted;
+            _storyTimers.Add(storyTimer);
         }
 
-        private void TextBox2_TextChanged(object sender, EventArgs e)
+        private void StoryTimer_TimerStarted(object sender, TimerEventArgs e)
         {
+            if (!checkBox1.Checked) return;
+            //Loop through and stop timers except the one started
+            foreach (var storyTimer in _storyTimers)
+            {
+                if (storyTimer.Id != e.Id)
+                {
+                    storyTimer.Stop();
+                }
+            }
+        }
 
+        private void TextBoxNew_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                StoryTimerInstance storyTimer = _storyTimers.Last();
+                if (_storyTimers.Count == 1 && !storyTimer.Panel.Visible)
+                {
+                    
+                    storyTimer.Title.Text = textBoxNew.Text;
+                    storyTimer.Panel.Visible = true;
+                    this.Height = this.Height + storyTimer.Panel.Height;
+                    storyTimer.Start();
+                }
+                else
+                {
+                    CreateNewTimer(textBoxNew.Text);
+                }
+                textBoxNew.Text = "";
+            }
+        }
+
+        private void CreateNewTimer(string title)
+        {
+            StoryTimerInstance lastTimer = _storyTimers.Last();
+            Panel lastPanel = lastTimer.Panel;
+            int id = _storyTimers.Count;
+            int top = lastPanel.Bottom + 10;
+            StoryTimerInstance newTimer = lastTimer.Clone(id, top);
+            _storyTimers.Add(newTimer);
+            newTimer.TimerStarted += StoryTimer_TimerStarted;
+            newTimer.Title.Text = title;
+            this.Height = this.Height + lastTimer.Panel.Height + 10;
+            this.Controls.Add(newTimer.Panel);
+            newTimer.Start();
+        }
+
+
+    }
+
+    public class TimerEventArgs: EventArgs
+    {
+        public int Id { get; set; }
+    }
+
+    public class StoryTimerInstance
+    {
+        public event EventHandler<TimerEventArgs> TimerStarted;
+        protected virtual void OnTimerStarted()
+        {
+            TimerEventArgs e = new TimerEventArgs() { Id = this.Id };
+            EventHandler<TimerEventArgs> handler = TimerStarted;
+            handler?.Invoke(this, e);
+        }
+
+        public int Id { get; set; }
+        public Panel Panel { get; set; } = new Panel();
+        public TextBox Title { get { return GetPanelControl("textBoxTitle") as TextBox; } }
+        public Button StartPause { get { return GetPanelControl("buttonStartPause") as Button; } }
+        public Button Reset { get { return GetPanelControl("buttonReset") as Button; } }
+        public RichTextBox ElapsedTime { get { return GetPanelControl("richTextBoxElapsedTime") as RichTextBox; } }
+        public System.Windows.Forms.Timer Timer { get; set; } = new System.Windows.Forms.Timer();
+        public int ElapsedSeconds { get; set; }
+
+        public StoryTimerInstance(int id, Panel panel)
+        {
+            Id = id;
+            Panel = panel;
+            Timer.Interval = 1000;
+            Timer.Tick += Timer_Tick;
+            StartPause.Click += PlayPause_Click;
+            Reset.Click += Reset_Click;
+            
+        }
+
+        private object GetPanelControl(string key)
+        {
+            try
+            {
+                return Panel.Controls[key];
+            }
+            catch { return null; }
+        }
+
+        /// <summary>
+        /// Starts the timer and sets the button color. Use this instead
+        /// of Timer.Start
+        /// </summary>
+        public void Start()
+        {
+            Timer.Start();
+            UpdateStartButton();
+            OnTimerStarted();
+        }
+
+        /// <summary>
+        /// Stops the timer and sets the button color. Use this instead of Timer.Stop().
+        /// </summary>
+        public void Stop()
+        {
+            Timer.Stop();
+            UpdateStartButton();
+        }
+
+        public void UpdateStartButton()
+        {
+            if (Timer.Enabled)
+            {
+                StartPause.BackColor = ElapsedTime.BackColor;
+                StartPause.ForeColor = ElapsedTime.ForeColor;
+            }
+            else
+            {
+                StartPause.BackColor = Reset.BackColor;
+                StartPause.ForeColor = Reset.ForeColor;
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Timer.Stop();
+            ElapsedSeconds = 0;
+            UpdateElapsedTime();
+            UpdateStartButton();
+        }
+
+        private void PlayPause_Click(object sender, EventArgs e)
+        {
+            Timer.Enabled = !Timer.Enabled;
+            UpdateElapsedTime();
+            UpdateStartButton();
+            if (Timer.Enabled) { OnTimerStarted(); }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ElapsedSeconds += 1;
+            UpdateElapsedTime();
+        }
+
+        private void UpdateElapsedTime()
+        {
+            ElapsedTime.Text = " " + TimeSpan.FromSeconds(ElapsedSeconds).ToString(@"h\:mm\:ss");
+        }
+
+        public StoryTimerInstance Clone(int id, int top)
+        {
+            Panel panel = new Panel();
+
+            panel.Top = top;
+            panel.Width = Panel.Width;
+            panel.Height = Panel.Height;
+            panel.Left = Panel.Left;
+
+            TextBox title = new TextBox();
+            title.Name = Title.Name;
+            title.Location = Title.Location;
+            title.Width = Title.Width;
+            title.Height = Title.Height;
+            panel.Controls.Add(title);
+
+            Button startPause = new Button();
+            startPause.Name = StartPause.Name;
+            startPause.Location = StartPause.Location;
+            startPause.Width = StartPause.Width;
+            startPause.Height = StartPause.Height;
+            startPause.Font = StartPause.Font;
+            startPause.Text = StartPause.Text;
+            panel.Controls.Add(startPause);
+
+            Button reset = new Button();
+            reset.Name = Reset.Name;
+            reset.Location = Reset.Location;
+            reset.Width = Reset.Width;
+            reset.Height = Reset.Height;
+            reset.Font = Reset.Font;
+            reset.Text = Reset.Text;
+            panel.Controls.Add(reset);
+
+            RichTextBox elapsedTime = new RichTextBox();
+            elapsedTime.Name = ElapsedTime.Name;
+            elapsedTime.Location = ElapsedTime.Location;
+            elapsedTime.Width = ElapsedTime.Width;
+            elapsedTime.Height = ElapsedTime.Height;
+            elapsedTime.Font = ElapsedTime.Font;
+            elapsedTime.ScrollBars = ElapsedTime.ScrollBars;
+            elapsedTime.BorderStyle = ElapsedTime.BorderStyle;
+            elapsedTime.BackColor = ElapsedTime.BackColor;
+            elapsedTime.ForeColor = ElapsedTime.ForeColor;
+            elapsedTime.Text = " 0:00:00";
+            panel.Controls.Add(elapsedTime);
+
+            StoryTimerInstance clone = new StoryTimerInstance(id, panel);
+            clone.Timer.Tag = id;
+            return clone;
         }
     }
 }
