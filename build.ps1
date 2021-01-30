57c76d3edc6ea0d47f223595e1a9720b473b53af
Charles L Flatt <charles@tfhome.us>
2021-01-30 12:47:06 -0700

    Add prompting for version in build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
 Write-Host "Build release version" -ForegroundColor Green
 dotnet publish src -c Release -o build
 Write-Host "Create zip archive" -ForegroundColor Green
-Compress-7Zip -ArchiveFileName storytimer.zip -Path build -OutputPath release
+$version = Read-Host -Prompt Version?
+Compress-7Zip -ArchiveFileName "StoryTimer-$version.zip" -Path build -OutputPath release
